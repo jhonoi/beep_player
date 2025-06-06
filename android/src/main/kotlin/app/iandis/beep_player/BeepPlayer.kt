856fe9ae860597c586ebb1ba4462451e8ff3e206
Jhonoi Tomlin <jhonoitomlin@Jhonois-MBP-M4.lan>
2025-06-06 11:01:01 -0500

    Fix BuildConfig reference and enable BuildConfig generation

--- android/src/main/kotlin/app/iandis/beep_player/BeepPlayer.kt
+++ android/src/main/kotlin/app/iandis/beep_player/BeepPlayer.kt
@@ -26,7 +26,7 @@ class BeepPlayer(
     private var _isDisposed: Boolean = false
 
     init {
-        if (BuildConfig.DEBUG) {
+        if (app.iandis.beep_player.BuildConfig.DEBUG) {
             _soundPool.setOnLoadCompleteListener { _, soundId: Int, status: Int ->
                 if (status != 0) {
                     Log.d(this::class.java.simpleName, "Failed to load sound id: $soundId")
